ebb6f7207532b1681cde47e56c3ce9c38ba5bec7
mizdra <pp.mizdra@gmail.com>
2024-09-22 19:53:02 +0900

    add 2-video

--- README.md
+++ README.md
@@ -6,5 +6,29 @@ See [#1](https://github.com/mizdra/bfcache-browser-compatibility-test/issues/1).
 
 ```bash
 node server.mjs
-open http://localhost:3000/1-basic.html
+open http://localhost:3000/1-fetch.html
+open http://localhost:3000/2-video.html
 ```
+
+## Comparison Result
+
+### Environment
+
+- macOS 14.6.1（23G93）
+- Chrome: 129.0.6668.59（Official Build） （arm64）
+- Safari: 17.6 (19618.3.11.11.5)
+- Firefox: 130.0.1 (64-bit)
+
+### 1-fetch.html
+
+
+
+### Chrome (`129.0.6668.59（Official Build） （arm64）`)
+
+## Browser Comparison: 2-video.html
+
+## Credits
+
+- `flower.webm`
+  - Copy from https://github.com/mdn/interactive-examples/blob/1c31eedaac0e6a69759e4fb4fffb3fb853e2df0b/live-examples/media/cc0-videos/flower.webm
+  - License: [CC0](https://github.com/mdn/interactive-examples/blob/1c31eedaac0e6a69759e4fb4fffb3fb853e2df0b/LICENSE)
--- server.mjs
+++ server.mjs
@@ -3,17 +3,31 @@ import http from 'http';
 import { setTimeout } from 'timers/promises';
 
 const server = http.createServer(async (req, res) => {
-  // handle /api//slow-response
+  // handle /api/slow-response
   if (req.url === '/api/slow-response') {
     await setTimeout(3000);
     res.end('The response of /api/slow-response');
     return;
   }
-  // handle /1-basic.html
-  if (req.url === '/1-basic.html') {
-    const html = await readFile('./1-basic.html', 'utf-8');
+  // handle /1-fetch.html
+  if (req.url === '/1-fetch.html') {
+    const html = await readFile('./1-fetch.html', 'utf-8');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(html);
+    return;
+  }
+  // handle /2-video.html
+  if (req.url === '/2-video.html') {
+    const html = await readFile('./2-video.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+    return;
+  }
+  // handle /flower.webm
+  if (req.url === '/flower.webm') {
+    const video = await readFile('./flower.webm');
+    res.writeHead(200, { 'Content-Type': 'video/webm' });
+    res.end(video);
     return;
   }
 })
